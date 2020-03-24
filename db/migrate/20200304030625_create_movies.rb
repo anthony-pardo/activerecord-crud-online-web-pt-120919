b404c846f2c74f947aa24bd45e7f7e0c6274d94e
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-24 18:06:37 +0000

    Automatically backed up by Learn

--- db/migrate/20200304030625_create_movies.rb
+++ db/migrate/20200304030625_create_movies.rb
@@ -6,6 +6,9 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.string :director 
       t.string :lead 
       t.boolean :in_theaters
+<<<<<<< HEAD
     end
+=======
+>>>>>>> eb5619d67257bb194b1366202d60c3bcc3e287cc
   end
 end
